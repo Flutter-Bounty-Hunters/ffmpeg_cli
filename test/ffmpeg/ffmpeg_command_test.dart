008cc3bd3b2f703e4935692cead26b07b742d7f8
Rutvik Tak <65209850+rutvik110@users.noreply.github.com>
2024-02-11 10:47:34 +0530

    moved ffmpeg use cases tests to new file

--- test/ffmpeg/ffmpeg_command_test.dart
+++ test/ffmpeg/ffmpeg_command_test.dart
@@ -113,42 +113,6 @@ void main() {
           ),
         );
       });
-
-      test("converts a group of images to a video", () {
-        final command = FfmpegCommand.simple(
-          inputs: [
-            // screenshot%d.png represents images named like screenshot0.png, screenshot1.png, screenshot2.png, etc.
-            FfmpegInput.asset("assets/images/screenshot%d.png"),
-
-            FfmpegInput.asset("assets/audio/audio.mp3"),
-          ],
-          args: [
-            const CliArg(name: 'y'),
-            const CliArg(name: 'c:a', value: 'aac'),
-            const CliArg(name: 'qscale:v', value: '1'),
-          ],
-          outputFilepath: 'assets/output/generated_video.mp4',
-        );
-
-        expect(
-          command.toCli(),
-          const CliCommand(
-            executable: 'ffmpeg',
-            args: [
-              "-i",
-              "assets/images/screenshot%d.png",
-              "-i",
-              "assets/audio/audio.mp3",
-              "-y",
-              "-c:a",
-              "aac",
-              "-qscale:v",
-              "1",
-              "assets/output/generated_video.mp4"
-            ],
-          ),
-        );
-      });
     });
   });
 }
