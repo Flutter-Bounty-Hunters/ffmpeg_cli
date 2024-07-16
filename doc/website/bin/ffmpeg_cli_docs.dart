77f0dc2be366cf5b3c9e300a1d254d615210964c
Matt Carroll <matthew-carroll@users.noreply.github.com>
2024-07-16 00:16:05 -0700

    [Docs] - switched to shared website assets

--- doc/website/bin/ffmpeg_cli_docs.dart
+++ doc/website/bin/ffmpeg_cli_docs.dart
@@ -5,11 +5,14 @@ import 'package:static_shock/static_shock.dart';
 Future<void> main(List<String> arguments) async {
   // Configure the static website generator.
   final staticShock = StaticShock()
-    // Here, you can directly hook into the StaticShock pipeline. For example,
-    // you can copy an "images" directory from the source set to build set:
+    // Pick source files.
     ..pick(DirectoryPicker.parse("images"))
-    // All 3rd party behavior is added through plugins, even the behavior
-    // shipped with Static Shock.
+    ..pickRemote(
+      layouts: _remoteLayouts,
+      components: _remoteComponents,
+      assets: _remoteAssets,
+    )
+    // Add all needed plugins.
     ..plugin(const MarkdownPlugin())
     ..plugin(const JinjaPlugin())
     ..plugin(const PrettyUrlsPlugin())
@@ -30,3 +33,80 @@ Future<void> main(List<String> arguments) async {
   // Generate the static website.
   await staticShock.generateSite();
 }
+
+final _remoteLayouts = <RemoteIncludeSource>{
+  // Main page layout.
+  RemoteInclude(
+    url:
+        "https://raw.githubusercontent.com/Flutter-Bounty-Hunters/fbh_branding/main/single-page-doc-sites/_includes/layouts/homepage.jinja?raw=true",
+    name: "homepage",
+  ),
+};
+
+final _remoteComponents = <RemoteIncludeSource>{
+  // Contributors component (used by main page layout).
+  RemoteInclude(
+    url:
+        "https://raw.githubusercontent.com/Flutter-Bounty-Hunters/fbh_branding/main/single-page-doc-sites/_includes/components/contributors.jinja?raw=true",
+    name: "contributors",
+  ),
+};
+
+final _remoteAssets = <RemoteFileSource>{
+  // Sass styles.
+  HttpFileGroup.fromUrlTemplate(
+    "https://raw.githubusercontent.com/Flutter-Bounty-Hunters/fbh_branding/main/single-page-doc-sites/styles/\$",
+    buildDirectory: "styles/",
+    files: {
+      "theme.scss",
+      "homepage.scss",
+    },
+  ),
+
+  // Flutter and Dart logos.
+  RemoteFile(
+    url:
+        "https://raw.githubusercontent.com/Flutter-Bounty-Hunters/fbh_branding/main/single-page-doc-sites/images/google/dart-logo.svg?raw=true",
+    buildPath: FileRelativePath("images/branding/", "dart-logo", "svg"),
+  ),
+  RemoteFile(
+    url:
+        "https://raw.githubusercontent.com/Flutter-Bounty-Hunters/fbh_branding/main/single-page-doc-sites/images/google/flutter-logo.svg?raw=true",
+    buildPath: FileRelativePath("images/branding/", "flutter-logo", "svg"),
+  ),
+
+  // Dart Favicons.
+  HttpFileGroup.fromUrlTemplate(
+    "https://github.com/Flutter-Bounty-Hunters/fbh_branding/blob/main/single-page-doc-sites/images/google/dart-favicon/\$?raw=true",
+    buildDirectory: "images/favicon/dart/",
+    files: {
+      "android-chrome-192x192.png",
+      "android-chrome-512x512.png",
+      "apple-touch-icon.png",
+      "browserconfig.xml",
+      "favicon-16x16.png",
+      "favicon-32x32.png",
+      "favicon.ico",
+      "mstile-150x150.png",
+      "site.webmanifest",
+    },
+  ),
+
+  // Flutter Favicons.
+  HttpFileGroup.fromUrlTemplate(
+    "https://github.com/Flutter-Bounty-Hunters/fbh_branding/blob/main/single-page-doc-sites/images/google/flutter-favicon/\$?raw=true",
+    buildDirectory: "images/favicon/flutter/",
+    files: {
+      "android-chrome-192x192.png",
+      "android-chrome-512x512.png",
+      "apple-touch-icon.png",
+      "browserconfig.xml",
+      "favicon-16x16.png",
+      "favicon-32x32.png",
+      "favicon.ico",
+      "mstile-150x150.png",
+      "safari-pinned-tab.svg",
+      "site.webmanifest",
+    },
+  ),
+};
